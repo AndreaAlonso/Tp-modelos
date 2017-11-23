59a21fe9c18c79b06390cef2db3d6aa500034601
xMagnomx <32860116+xMagnomx@users.noreply.github.com>
2017-11-23 17:47:05 -0300

    FlyWeight
    jejeeje

---
+++ Assets/Scripts/FlyWeightPointer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FlyWeightPointer
+{
+    public static readonly FlyWeight State = new FlyWeight()
+    {
+        hpMax = 100,
+        speed = 5
+    };
+
+}
