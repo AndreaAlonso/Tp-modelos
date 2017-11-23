bfb1c73f50d310923e5152a8e883bc6908d05d1d
AndreaAlonso <32860571+AndreaAlonso@users.noreply.github.com>
2017-11-23 17:54:53 -0300

    My branch

---
+++ Assets/Scripts/ModelPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelPlayer {
+
+    private Transform _playerTransform;
+    public int hp;
+
+    public ModelPlayer (Transform t)
+    {
+        _playerTransform = t;
+    }
+}
